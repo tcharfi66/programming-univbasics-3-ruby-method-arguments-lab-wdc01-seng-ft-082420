a9baf525aad9b0d51e7637b0615f3a472d8513b1
Tej Charfi <charfi@usc.edu>
2020-08-15 19:50:20 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -12,11 +12,11 @@ end
 
 
 def introduction_with_language(name, lang)
-  puts ("Hi, my name is "+ name + "and I am learning to program in" + lang + ".")
+  puts "Hi, my name is ${name} and I am learning to program in ${lang}."
 end
 
 def introduction_with_language_optional(name, lang = "Ruby")
-  puts ("Hi, my name is" + name "and I am learning to program in" + lang + ".")
+  puts "Hi, my name is ${name} and I am learning to program in ${lang}."
 end
 
 
